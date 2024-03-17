a863fbf1175c625416cc3f7461f36be9d6925b22
Dan Nascimento <dannascimento93@gmail.com>
2024-03-17 03:50:13 -0400

    Add completed P8-4

--- CIS234_webdev2/Chapter8/project04/project08-04.js
+++ CIS234_webdev2/Chapter8/project04/project08-04.js
@@ -4,8 +4,8 @@
       Project 08-04
 
       Retrieve Staff Data from a JSON File
-      Author: 
-      Date:   
+      Author: Dan Nascimento
+      Date:   03/17/2024
 
       Filename: project08-04.js
 */
@@ -23,8 +23,9 @@ getFileButton.onchange = function() {
    fr.readAsText(JSONfile); 
 
    // Once the file has finished loading, parse the JSON file
-   fr.onload=function(){ 
-
+   fr.onload = function(){ 
+      let staff = JSON.parse(fr.result);
+      makeStaffTable(staff);
    }
    
 };
@@ -32,6 +33,21 @@ getFileButton.onchange = function() {
 function makeStaffTable(staff) {
    let staffTable = document.createElement("table");
    let headerRow = document.createElement("tr");
-   
+   for (let header in staff.directory[0]) {
+      let headerCell = document.createElement("th");
+      headerCell.textContent = header;
+      headerRow.appendChild(headerCell);
+   }
+   staffTable.appendChild(headerRow);
 
+   for (let row in staff.directory) {
+      let tableRow = document.createElement("tr");
+      for (let column in staff.directory[row]) {
+         let tableCell = document.createElement("td");
+         tableCell.textContent = staff.directory[row][column];
+         tableRow.appendChild(tableCell);
+      }
+      staffTable.appendChild(tableRow);
+   }
+   containerBox.appendChild(staffTable);
 }
